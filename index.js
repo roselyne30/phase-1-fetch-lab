17c83801630045f617ca76c2507f0ba62ece28fc
roselynelutomia <roselyne.lutomia@student.moringaschool.com>
2023-01-12 23:27:36 +0300

    MY Completed Assignment

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
@@ -11,7 +13,7 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
+ 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
